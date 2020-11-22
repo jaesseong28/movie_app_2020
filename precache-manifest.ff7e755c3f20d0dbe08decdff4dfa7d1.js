64577b3ed76d76b1a21e157346b300e4103fce84
JaeSeong Jeong <jaeseongjeong@JaeSeongui-iMac.local>
2020-11-22 23:57:31 +0900

    Updates

---
+++ precache-manifest.ff7e755c3f20d0dbe08decdff4dfa7d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e7b4f78ae161009c009f1152c10d07",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "88d504875469bec0ec91",
+    "url": "/movie_app_2020/static/css/main.6325a078.chunk.css"
+  },
+  {
+    "revision": "dec5d34ecf7b10a589c0",
+    "url": "/movie_app_2020/static/js/2.a38c7355.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.a38c7355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88d504875469bec0ec91",
+    "url": "/movie_app_2020/static/js/main.70a5b289.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
